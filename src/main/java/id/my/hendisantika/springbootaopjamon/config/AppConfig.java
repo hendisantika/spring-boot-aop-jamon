2771c44613147a70d76354a39c9a607ed5f843a3
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 06:06:22 +0700

    feat: Add jdbcTemplate in AppConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootaopjamon/config/AppConfig.java
+++ src/main/java/id/my/hendisantika/springbootaopjamon/config/AppConfig.java
@@ -5,7 +5,10 @@ import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCusto
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +32,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
