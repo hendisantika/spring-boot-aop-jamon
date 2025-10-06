e388fb83fd0dc8fddd8beeab53cd21724afbcd45
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 06:09:32 +0700

    feat: Add BookVolume Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootaopjamon/model/BookVolume.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.springbootaopjamon.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-aop-jamon
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 07/10/25
+ * Time: 06.09
+ * To change this template use File | Settings | File Templates.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record BookVolume(VolumeInfo volumeInfo) {
+}
