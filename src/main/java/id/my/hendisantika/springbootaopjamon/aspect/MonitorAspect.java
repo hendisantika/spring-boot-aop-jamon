edff9122996bac9a9b7faec7395971ee4e179cd4
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 06:04:29 +0700

    feat: Add monitorMethodExecution in MonitorAspect🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootaopjamon/aspect/MonitorAspect.java
+++ src/main/java/id/my/hendisantika/springbootaopjamon/aspect/MonitorAspect.java
@@ -1,8 +1,14 @@
 package id.my.hendisantika.springbootaopjamon.aspect;
 
+import com.jamonapi.Monitor;
 import com.jamonapi.MonitorFactoryInterface;
+import id.my.hendisantika.springbootaopjamon.annotation.MethodMonitor;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -25,4 +31,22 @@ public class MonitorAspect {
     private final MonitorFactoryInterface monitorFactoryInterface;
     private final MeterRegistry meterRegistry;
 
+    @Around(value = "controllerMethod() && @annotation(methodMonitor)")
+    public Object monitorMethodExecution(ProceedingJoinPoint joinPoint, MethodMonitor methodMonitor) throws Throwable {
+        Monitor monitor = monitorFactoryInterface.start(methodMonitor.name());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            monitor.stop();
+            Counter.builder("http.requests.count")
+                    .tag("uri", methodMonitor.uri())
+                    .tag("method", methodMonitor.method())
+                    .register(meterRegistry).increment();
+            Gauge
+                    .builder("http.response.time", monitor::getLastValue)
+                    .tag("uri", methodMonitor.uri())
+                    .tag("method", methodMonitor.method())
+                    .register(meterRegistry);
+        }
+    }
 }
