f93e036f7663a21c6828c5dd151ec558ba03d72e
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 06:15:14 +0700

    feat: Add account in AccountController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootaopjamon/controller/AccountController.java
+++ src/main/java/id/my/hendisantika/springbootaopjamon/controller/AccountController.java
@@ -1,9 +1,16 @@
 package id.my.hendisantika.springbootaopjamon.controller;
 
+import id.my.hendisantika.springbootaopjamon.annotation.MethodMonitor;
+import id.my.hendisantika.springbootaopjamon.model.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +30,10 @@ public class AccountController {
 
     public final JdbcTemplate jdbcTemplate;
 
+    @GetMapping
+    @MethodMonitor(name = "account.list", uri = "/account")
+    public ResponseEntity<List<Account>> account() {
+        List<Account> account = jdbcTemplate.query("SELECT * FROM account", new BeanPropertyRowMapper(Account.class));
+        return ResponseEntity.ok(account);
+    }
 }
