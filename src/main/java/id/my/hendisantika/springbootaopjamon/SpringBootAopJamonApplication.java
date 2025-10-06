8753208a49ae18d30270d873c662739a9a94f84d
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 05:59:49 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootaopjamon/SpringBootAopJamonApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootaopjamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopJamonApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootAopJamonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootaopjamon/SpringBootAopJamonApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootaopjamon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAopJamonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
