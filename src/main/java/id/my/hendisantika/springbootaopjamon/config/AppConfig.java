cf9792d8d9988b703a0f9e8e41b87731f7106004
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 06:05:33 +0700

    feat: Add AppConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootaopjamon/config/AppConfig.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.springbootaopjamon.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-aop-jamon
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 07/10/25
+ * Time: 06.05
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@ComponentScan(basePackages = {"id.my.hendisantika.springbootaopjamon.controller", "id.my.hendisantika.springbootaopjamon.service"})
+public class AppConfig {
+}
