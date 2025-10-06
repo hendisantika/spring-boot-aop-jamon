d658ec3c2e86bf61427fb4d5306a0b71030668ec
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 06:10:56 +0700

    feat: Add BookService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootaopjamon/service/BookService.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.springbootaopjamon.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-aop-jamon
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 07/10/25
+ * Time: 06.10
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class BookService {
+    private final RestTemplate restTemplate;
+}
